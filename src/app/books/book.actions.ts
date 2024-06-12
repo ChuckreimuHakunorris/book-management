54734dd9f5606a3098e2f46216362bd90e2bd8cb
Ville Laaksonen <leligens@gmail.com>
2024-06-12 17:44:53 +0300

    adde book effects, completed store functionality for books

--- src/app/books/book.actions.ts
+++ src/app/books/book.actions.ts
@@ -3,6 +3,16 @@ import { Book } from '../models/book';
 
 export const AddBook = createAction('[Book] Add Book', props<Book>());
 
+export const AddBookSuccess = createAction(
+  '[Book] Add Book Succesfully',
+  props<Book>()
+);
+
+export const AddBookFailure = createAction(
+  '[Book] Add Book Failure',
+  props<{ error: any }>()
+);
+
 export const RemoveBook = createAction(
   '[Book] Remove Book',
   props<{ bookId: string }>()
